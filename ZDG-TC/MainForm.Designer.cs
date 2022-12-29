f72c7260d829eaee819649d2e2b68a54f11cc6d5
YuzuRyo61 <yuzuryo61@yuzulia.work>
2022-12-29 16:41:52 +0900

    ボタン割り当て可能にしたり、レバーサに対応したりなど

--- ZDG-TC/MainForm.Designer.cs
+++ ZDG-TC/MainForm.Designer.cs
@@ -46,6 +46,29 @@
             this.DeviceListLabel = new System.Windows.Forms.Label();
             this.GamepadComboBox = new System.Windows.Forms.ComboBox();
             this.MasconLevel = new System.Windows.Forms.Label();
+            this.MasconLevelLabel = new System.Windows.Forms.Label();
+            this.ButtonACombo = new System.Windows.Forms.ComboBox();
+            this.ButtonALabel = new System.Windows.Forms.Label();
+            this.ButtonBLabel = new System.Windows.Forms.Label();
+            this.ButtonBCombo = new System.Windows.Forms.ComboBox();
+            this.ButtonXLabel = new System.Windows.Forms.Label();
+            this.ButtonXCombo = new System.Windows.Forms.ComboBox();
+            this.ButtonYLabel = new System.Windows.Forms.Label();
+            this.ButtonYCombo = new System.Windows.Forms.ComboBox();
+            this.ButtonPlusLabel = new System.Windows.Forms.Label();
+            this.ButtonPlusCombo = new System.Windows.Forms.ComboBox();
+            this.ButtonMinusLabel = new System.Windows.Forms.Label();
+            this.ButtonMinusCombo = new System.Windows.Forms.ComboBox();
+            this.ButtonLLabel = new System.Windows.Forms.Label();
+            this.ButtonLCombo = new System.Windows.Forms.ComboBox();
+            this.ButtonRLabel = new System.Windows.Forms.Label();
+            this.ButtonRCombo = new System.Windows.Forms.ComboBox();
+            this.ButtonZLLabel = new System.Windows.Forms.Label();
+            this.ButtonZRLabel = new System.Windows.Forms.Label();
+            this.ButtonZRCombo = new System.Windows.Forms.ComboBox();
+            this.ButtonZLReservedLabel = new System.Windows.Forms.Label();
+            this.ReverserLabel = new System.Windows.Forms.Label();
+            this.ReverserValue = new System.Windows.Forms.Label();
             this.MainMenu.SuspendLayout();
             this.StatusBar.SuspendLayout();
             this.SuspendLayout();
@@ -153,10 +176,189 @@
             resources.ApplyResources(this.MasconLevel, "MasconLevel");
             this.MasconLevel.Name = "MasconLevel";
             // 
+            // MasconLevelLabel
+            // 
+            resources.ApplyResources(this.MasconLevelLabel, "MasconLevelLabel");
+            this.MasconLevelLabel.Name = "MasconLevelLabel";
+            // 
+            // ButtonACombo
+            // 
+            resources.ApplyResources(this.ButtonACombo, "ButtonACombo");
+            this.ButtonACombo.DisplayMember = "Name";
+            this.ButtonACombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ButtonACombo.FormattingEnabled = true;
+            this.ButtonACombo.Name = "ButtonACombo";
+            this.ButtonACombo.ValueMember = "Action";
+            // 
+            // ButtonALabel
+            // 
+            resources.ApplyResources(this.ButtonALabel, "ButtonALabel");
+            this.ButtonALabel.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.ButtonALabel.Name = "ButtonALabel";
+            // 
+            // ButtonBLabel
+            // 
+            resources.ApplyResources(this.ButtonBLabel, "ButtonBLabel");
+            this.ButtonBLabel.Name = "ButtonBLabel";
+            // 
+            // ButtonBCombo
+            // 
+            resources.ApplyResources(this.ButtonBCombo, "ButtonBCombo");
+            this.ButtonBCombo.DisplayMember = "Name";
+            this.ButtonBCombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ButtonBCombo.FormattingEnabled = true;
+            this.ButtonBCombo.Name = "ButtonBCombo";
+            this.ButtonBCombo.ValueMember = "Action";
+            // 
+            // ButtonXLabel
+            // 
+            resources.ApplyResources(this.ButtonXLabel, "ButtonXLabel");
+            this.ButtonXLabel.Name = "ButtonXLabel";
+            // 
+            // ButtonXCombo
+            // 
+            resources.ApplyResources(this.ButtonXCombo, "ButtonXCombo");
+            this.ButtonXCombo.DisplayMember = "Name";
+            this.ButtonXCombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ButtonXCombo.FormattingEnabled = true;
+            this.ButtonXCombo.Name = "ButtonXCombo";
+            this.ButtonXCombo.ValueMember = "Action";
+            // 
+            // ButtonYLabel
+            // 
+            resources.ApplyResources(this.ButtonYLabel, "ButtonYLabel");
+            this.ButtonYLabel.Name = "ButtonYLabel";
+            // 
+            // ButtonYCombo
+            // 
+            resources.ApplyResources(this.ButtonYCombo, "ButtonYCombo");
+            this.ButtonYCombo.DisplayMember = "Name";
+            this.ButtonYCombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ButtonYCombo.FormattingEnabled = true;
+            this.ButtonYCombo.Name = "ButtonYCombo";
+            this.ButtonYCombo.ValueMember = "Action";
+            // 
+            // ButtonPlusLabel
+            // 
+            resources.ApplyResources(this.ButtonPlusLabel, "ButtonPlusLabel");
+            this.ButtonPlusLabel.Name = "ButtonPlusLabel";
+            // 
+            // ButtonPlusCombo
+            // 
+            resources.ApplyResources(this.ButtonPlusCombo, "ButtonPlusCombo");
+            this.ButtonPlusCombo.DisplayMember = "Name";
+            this.ButtonPlusCombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ButtonPlusCombo.FormattingEnabled = true;
+            this.ButtonPlusCombo.Name = "ButtonPlusCombo";
+            this.ButtonPlusCombo.ValueMember = "Action";
+            // 
+            // ButtonMinusLabel
+            // 
+            resources.ApplyResources(this.ButtonMinusLabel, "ButtonMinusLabel");
+            this.ButtonMinusLabel.Name = "ButtonMinusLabel";
+            // 
+            // ButtonMinusCombo
+            // 
+            resources.ApplyResources(this.ButtonMinusCombo, "ButtonMinusCombo");
+            this.ButtonMinusCombo.DisplayMember = "Name";
+            this.ButtonMinusCombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ButtonMinusCombo.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.ButtonMinusCombo.FormattingEnabled = true;
+            this.ButtonMinusCombo.Name = "ButtonMinusCombo";
+            this.ButtonMinusCombo.ValueMember = "Action";
+            // 
+            // ButtonLLabel
+            // 
+            resources.ApplyResources(this.ButtonLLabel, "ButtonLLabel");
+            this.ButtonLLabel.Name = "ButtonLLabel";
+            // 
+            // ButtonLCombo
+            // 
+            resources.ApplyResources(this.ButtonLCombo, "ButtonLCombo");
+            this.ButtonLCombo.DisplayMember = "Name";
+            this.ButtonLCombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ButtonLCombo.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.ButtonLCombo.FormattingEnabled = true;
+            this.ButtonLCombo.Name = "ButtonLCombo";
+            this.ButtonLCombo.ValueMember = "Action";
+            // 
+            // ButtonRLabel
+            // 
+            resources.ApplyResources(this.ButtonRLabel, "ButtonRLabel");
+            this.ButtonRLabel.Name = "ButtonRLabel";
+            // 
+            // ButtonRCombo
+            // 
+            resources.ApplyResources(this.ButtonRCombo, "ButtonRCombo");
+            this.ButtonRCombo.DisplayMember = "Name";
+            this.ButtonRCombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ButtonRCombo.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.ButtonRCombo.FormattingEnabled = true;
+            this.ButtonRCombo.Name = "ButtonRCombo";
+            this.ButtonRCombo.ValueMember = "Action";
+            // 
+            // ButtonZLLabel
+            // 
+            resources.ApplyResources(this.ButtonZLLabel, "ButtonZLLabel");
+            this.ButtonZLLabel.Name = "ButtonZLLabel";
+            // 
+            // ButtonZRLabel
+            // 
+            resources.ApplyResources(this.ButtonZRLabel, "ButtonZRLabel");
+            this.ButtonZRLabel.Name = "ButtonZRLabel";
+            // 
+            // ButtonZRCombo
+            // 
+            resources.ApplyResources(this.ButtonZRCombo, "ButtonZRCombo");
+            this.ButtonZRCombo.DisplayMember = "Name";
+            this.ButtonZRCombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ButtonZRCombo.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.ButtonZRCombo.FormattingEnabled = true;
+            this.ButtonZRCombo.Name = "ButtonZRCombo";
+            this.ButtonZRCombo.ValueMember = "Action";
+            // 
+            // ButtonZLReservedLabel
+            // 
+            resources.ApplyResources(this.ButtonZLReservedLabel, "ButtonZLReservedLabel");
+            this.ButtonZLReservedLabel.Name = "ButtonZLReservedLabel";
+            // 
+            // ReverserLabel
+            // 
+            resources.ApplyResources(this.ReverserLabel, "ReverserLabel");
+            this.ReverserLabel.Name = "ReverserLabel";
+            // 
+            // ReverserValue
+            // 
+            resources.ApplyResources(this.ReverserValue, "ReverserValue");
+            this.ReverserValue.Name = "ReverserValue";
+            // 
             // MainForm
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.ReverserLabel);
+            this.Controls.Add(this.ReverserValue);
+            this.Controls.Add(this.ButtonZLReservedLabel);
+            this.Controls.Add(this.ButtonZRLabel);
+            this.Controls.Add(this.ButtonZRCombo);
+            this.Controls.Add(this.ButtonZLLabel);
+            this.Controls.Add(this.ButtonRLabel);
+            this.Controls.Add(this.ButtonRCombo);
+            this.Controls.Add(this.ButtonLLabel);
+            this.Controls.Add(this.ButtonLCombo);
+            this.Controls.Add(this.ButtonMinusLabel);
+            this.Controls.Add(this.ButtonMinusCombo);
+            this.Controls.Add(this.ButtonPlusLabel);
+            this.Controls.Add(this.ButtonPlusCombo);
+            this.Controls.Add(this.ButtonYLabel);
+            this.Controls.Add(this.ButtonYCombo);
+            this.Controls.Add(this.ButtonXLabel);
+            this.Controls.Add(this.ButtonXCombo);
+            this.Controls.Add(this.ButtonBLabel);
+            this.Controls.Add(this.ButtonBCombo);
+            this.Controls.Add(this.ButtonALabel);
+            this.Controls.Add(this.ButtonACombo);
+            this.Controls.Add(this.MasconLevelLabel);
             this.Controls.Add(this.MasconLevel);
             this.Controls.Add(this.GamepadComboBox);
             this.Controls.Add(this.DeviceListLabel);
@@ -195,6 +397,29 @@
         private System.Windows.Forms.Label DeviceListLabel;
         private System.Windows.Forms.ComboBox GamepadComboBox;
         private System.Windows.Forms.Label MasconLevel;
+        private System.Windows.Forms.Label MasconLevelLabel;
+        private System.Windows.Forms.ComboBox ButtonACombo;
+        private System.Windows.Forms.Label ButtonALabel;
+        private System.Windows.Forms.Label ButtonBLabel;
+        private System.Windows.Forms.ComboBox ButtonBCombo;
+        private System.Windows.Forms.Label ButtonXLabel;
+        private System.Windows.Forms.ComboBox ButtonXCombo;
+        private System.Windows.Forms.Label ButtonYLabel;
+        private System.Windows.Forms.ComboBox ButtonYCombo;
+        private System.Windows.Forms.Label ButtonPlusLabel;
+        private System.Windows.Forms.ComboBox ButtonPlusCombo;
+        private System.Windows.Forms.Label ButtonMinusLabel;
+        private System.Windows.Forms.ComboBox ButtonMinusCombo;
+        private System.Windows.Forms.Label ButtonLLabel;
+        private System.Windows.Forms.ComboBox ButtonLCombo;
+        private System.Windows.Forms.Label ButtonRLabel;
+        private System.Windows.Forms.ComboBox ButtonRCombo;
+        private System.Windows.Forms.Label ButtonZLLabel;
+        private System.Windows.Forms.Label ButtonZRLabel;
+        private System.Windows.Forms.ComboBox ButtonZRCombo;
+        private System.Windows.Forms.Label ButtonZLReservedLabel;
+        private System.Windows.Forms.Label ReverserLabel;
+        private System.Windows.Forms.Label ReverserValue;
     }
 }
 
--- ZDG-TC/MainForm.cs
+++ ZDG-TC/MainForm.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Windows.Forms;
 
 using SharpDX;
@@ -10,10 +11,22 @@ namespace ZDG_TC
 {
     public partial class MainForm : Form
     {
+        // コントローラーが押されている場合と押されていない場合の色の値
+        private Color CONTROLLER_ON_COLOR { get; } = Color.Red;
+        private Color CONTROLLER_OFF_COLOR { get; } = SystemColors.ControlText;
+
+        // バージョンフォームクラス
         private VersionForm versionForm;
+        // DirectInput用
         private DirectInput di;
+        // ジョイスティック
         private Joystick joystick;
+        // 前回押された十字パッド
+        private int previousPOVValue = -1;
+        // レバーサ位置 (1: 前進, 0: 中立, -1: 後進)
+        private int reverserDir = 1;
         
+        // コントローラーコンボボックス用のリストアイテムクラス
         public class ControllerItem
         {
             public string Name { set; get; }
@@ -26,6 +39,45 @@ namespace ZDG_TC
             }
         }
 
+        public class SWEnumItem
+        {
+            public string Name { set; get; }
+            public InputAction? Action { set; get; }
+
+            public SWEnumItem(string n, InputAction? a)
+            {
+                Name = n;
+                Action = a;
+            }
+
+            public static List<SWEnumItem> GetList()
+            {
+                return new List<SWEnumItem>() {
+                    new SWEnumItem("(未設定)", null),
+                    new SWEnumItem("力行側へ1段", InputAction.NotchUp),
+                    new SWEnumItem("制動側へ1段", InputAction.NotchDw),
+                    new SWEnumItem("ノッチをNにする", InputAction.NotchN),
+                    new SWEnumItem("ノッチをN側へ1段", InputAction.NotchToN),
+                    new SWEnumItem("EBノッチ", InputAction.NotchEB),
+                    new SWEnumItem("B1ノッチ", InputAction.NotchB1),
+                    new SWEnumItem("勾配起動", InputAction.GradientStart),
+                    new SWEnumItem("ブザー", InputAction.Buzzer),
+                    new SWEnumItem("空笛", InputAction.HornAir),
+                    new SWEnumItem("電笛", InputAction.HornEle),
+                    new SWEnumItem("外部視点との切り替え", InputAction.ViewChange),
+                    new SWEnumItem("ポーズメニュー", InputAction.PauseMenu),
+                    new SWEnumItem("スタフの表示・非表示", InputAction.ViewDiagram),
+                    new SWEnumItem("UIの表示・非表示", InputAction.ViewUserInterface),
+                    new SWEnumItem("視点の回転をリセット", InputAction.ViewHome),
+                    new SWEnumItem("EBリセット", InputAction.EBReset),
+                    new SWEnumItem("右側へ視点移動", InputAction.DriverViewR),
+                    new SWEnumItem("左側へ視点移動", InputAction.DriverViewL),
+                    new SWEnumItem("運転席へ視点移動", InputAction.DriverViewC),
+                };
+            }
+        }
+
+        // コンストラクタ
         public MainForm()
         {
             InitializeComponent();
@@ -34,6 +86,40 @@ namespace ZDG_TC
 
             // DirectInputの初期化
             di = new DirectInput();
+
+            // コントローラーの割当のコンボボックスの設定 ↓
+            // Aボタン
+            ButtonACombo.DataSource = SWEnumItem.GetList();
+            ButtonACombo.SelectedValue = InputAction.HornEle; // 電笛
+
+            // Bボタン
+            ButtonBCombo.DataSource = SWEnumItem.GetList();
+            ButtonBCombo.SelectedValue = InputAction.Buzzer; // ブザー
+
+            // Xボタン
+            ButtonXCombo.DataSource = SWEnumItem.GetList();
+            ButtonXCombo.SelectedValue = InputAction.HornAir; // 空笛
+
+            // Yボタン
+            ButtonYCombo.DataSource = SWEnumItem.GetList();
+            ButtonYCombo.SelectedValue = InputAction.EBReset; // EBリセット
+
+            // Lボタン
+            ButtonLCombo.DataSource = SWEnumItem.GetList();
+
+            // Rボタン
+            ButtonRCombo.DataSource = SWEnumItem.GetList();
+
+            // +ボタン
+            ButtonPlusCombo.DataSource = SWEnumItem.GetList();
+            ButtonPlusCombo.SelectedValue = InputAction.PauseMenu;
+
+            // -ボタン
+            ButtonMinusCombo.DataSource = SWEnumItem.GetList();
+
+            // ZRボタン
+            ButtonZRCombo.DataSource = SWEnumItem.GetList();
+            // コントローラーの割当のコンボボックスの設定 ↑
         }
 
         // VersionFormの初期化
@@ -113,16 +199,18 @@ namespace ZDG_TC
         // タイマー処理
         private void MainTimer_Tick(object sender, EventArgs e)
         {
-            // TODO: コントローラーの処理
+            // ジョイスティックが認識されていない場合は処理しない
             if (joystick == null) return;
 
             try
             {
+                // ジョイスティックを認識
                 joystick.Acquire();
                 joystick.Poll();
             }
             catch
             {
+                // ジョイスティックの接続が切れた場合、破棄する
                 joystick?.Dispose();
                 joystick = null;
                 DetectController();
@@ -130,9 +218,11 @@ namespace ZDG_TC
                 return;
             }
 
+            // ジョイスティックの状態取得、できなければ無視
             var jState = joystick.GetCurrentState();
             if (jState == null) return;
-            // マスコン状態
+
+            // ノッチ制御
             switch (jState.Y)
             {
                 case int n when n <= -900 && jState.Buttons[6]: // EB
@@ -196,14 +286,97 @@ namespace ZDG_TC
                     break;
             }
 
+            // ボタン処理
+            ButtonAction(ButtonALabel, ButtonACombo, jState.Buttons[2]); // Aボタン
+            ButtonAction(ButtonBLabel, ButtonBCombo, jState.Buttons[1]); // Bボタン
+            ButtonAction(ButtonXLabel, ButtonXCombo, jState.Buttons[3]); // Xボタン
+            ButtonAction(ButtonYLabel, ButtonYCombo, jState.Buttons[0]); // Yボタン
+            ButtonAction(ButtonLLabel, ButtonLCombo, jState.Buttons[4]); // Lボタン
+            ButtonAction(ButtonRLabel, ButtonRCombo, jState.Buttons[5]); // Rボタン
+            ButtonAction(ButtonPlusLabel, ButtonPlusCombo, jState.Buttons[9]); // +ボタン
+            ButtonAction(ButtonMinusLabel, ButtonMinusCombo, jState.Buttons[8]); // -ボタン
+            ButtonAction(ButtonZRLabel, ButtonZRCombo, jState.Buttons[7]); // ZRボタン
+
+            // ZLボタンが押されているかの反応処理
+            if (jState.Buttons[6])
+            {
+                ButtonZLLabel.ForeColor = CONTROLLER_ON_COLOR;
+            }
+            else
+            {
+                ButtonZLLabel.ForeColor = CONTROLLER_OFF_COLOR;
+            }
+
+            // TrainCrewからの列車情報を取得
             TrainState ts = TrainCrewInput.GetTrainState();
             if (ts.diaName != "")
             {
-                StatusBarMessage.Text = string.Format("運転中: {0}列車 {1} {2}行き - {3}km/h", ts.diaName, ts.Class, ts.BoundFor, ts.Speed);
+                StatusBarMessage.Text = string.Format("{0}列車 {1} {2}行き - {3}km/h", ts.diaName, ts.Class, ts.BoundFor, ts.Speed);
             }
             else
             {
                 StatusBarMessage.Text = "準備完了";
+            }
+
+            // 十字パッド
+            if (jState.PointOfViewControllers[0] != -1 && previousPOVValue != jState.PointOfViewControllers[0])
+            {
+                previousPOVValue = jState.PointOfViewControllers[0];
+                switch (jState.PointOfViewControllers[0])
+                {
+                    case 0: // 上
+                        if (reverserDir >= 1) break;
+                        reverserDir++;
+                        break;
+                    case 18000: // 下
+                        if (reverserDir <= -1) break;
+                        reverserDir--;
+                        break;
+                }
+
+                switch (reverserDir)
+                {
+                    case 1: // 前進
+                        ReverserValue.Text = "前進";
+                        break;
+                    case 0: // 中立
+                        ReverserValue.Text = "中立";
+                        break;
+                    case -1: // 後進
+                        ReverserValue.Text = "後進";
+                        break;
+                    default: // 不明
+                        ReverserValue.Text = "?";
+                        break;
+                }
+            }
+            if (jState.PointOfViewControllers[0] == -1 && previousPOVValue != -1)
+            {
+                // 十字パッドのボタンが離されたときリセット
+                previousPOVValue = -1;
+            }
+
+            TrainCrewInput.SetReverser(reverserDir);
+        }
+
+        private void ButtonAction(Label label, ComboBox cb,  bool on)
+        {
+            // ボタンが押されていれば色を変える、押されていなければ元の色にする
+            if (on)
+            {
+                label.ForeColor = CONTROLLER_ON_COLOR;
+            }
+            else
+            {
+                label.ForeColor = CONTROLLER_OFF_COLOR;
+            }
+
+            // コンボボックスの選択しているアイテムを取得
+            SWEnumItem swei = (SWEnumItem) cb.SelectedItem;
+            // 「(未設定)」でなければそのアクションを実行
+            if (swei.Action != null)
+            {
+                TrainCrewInput.SetButton((InputAction)swei.Action, on);
             }
         }
 
